869df9c9fa1470f2b4bb46cd9d503ede30c44154
unknown <samuelsales1410@gmail.com>
2023-01-28 03:55:26 -0300

    carrousel1

--- src/app/modules/public/home-public/components/carousel/carousel.component.ts
+++ src/app/modules/public/home-public/components/carousel/carousel.component.ts
@@ -6,22 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
+  
   slides = [
-    { img: 'https://via.placeholder.com/600.png/09f/fff' },
-    { img: 'https://via.placeholder.com/600.png/021/fff' },
-    { img: 'https://via.placeholder.com/600.png/321/fff' },
-    { img: 'https://via.placeholder.com/600.png/422/fff' },
-    { img: 'https://via.placeholder.com/600.png/654/fff' },
-    { img: 'https://via.placeholder.com/600.png/654/fff' },
-    { img: 'https://via.placeholder.com/600.png/654/fff' },
-    { img: 'https://via.placeholder.com/600.png/654/fff' },
-    { img: 'https://via.placeholder.com/600.png/654/fff' }
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/10.png', ok:"Áudio e instrumentos"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/02.png', ok:"Celulares"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/03.png', ok:"Smart Tv's" },
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/04.png', ok:"Eletrodomésticos"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/05.png', ok:"Móveis" },
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/06.png', ok:"Informática e acessórios"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/07.png', ok:"Wearables" },
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/09.png', ok:"Eletroportáteis"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/26.png', ok:"Utilidade domésticas"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/10.png', ok:"Áudio e instrumentos"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/02.png', ok:"Celulares"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/03.png', ok:"Smart Tv's"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/04.png', ok:"Eletrodomésticos"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/05.png', ok:"Móveis"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/06.png', ok:"Informática e acessórios"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/07.png', ok:"Wearables" },
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/09.png', ok:"Eletroportáteis"},
+    { img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/26.png', ok:"Utilidade domésticas"},
   ];
+  
 
-  slideConfig = { slidesToShow: 6, slidesToScroll: 4, arrows: true };
+
+
+  slideConfig = { slidesToShow: 3, slidesToScroll: 9, arrows: true };
 
   addSlide() {
-    this.slides.push({ img: 'http://placehold.it/350x150/777777' });
+    this.slides.push({ img: 'https://www.casasbahia-imagens.com.br/criacao/01-home/icones-depto/2023/01-jan/27/10.png', ok:"Áudio e instrumentos" });
   }
 
   removeSlide() {
